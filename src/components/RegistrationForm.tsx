824885aa582e8ea0c8f32271dca1a20ca2312a23
jelisavac-l <jelisavacluka63@gmail.com>
2025-06-22 19:31:25 +0200

    chore(RegistrationForm): Standardized margins

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -4,6 +4,7 @@ export default function RegisterForm(){
     const [username, setUsername] = useState<string>('')
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
+    const [confirm, setConfirm] = useState<string>('')
 
     return (
         <div className="register-container">
@@ -12,13 +13,13 @@ export default function RegisterForm(){
             </div>
             <div className="register-input-container">
                 <h1 className="register-title">Sign up to Mercypher</h1>
-                <div className="register-subtitle ">
+                <div className="register-subtitle mb-1">
                     <p>Username</p>
                 </div>
                 <input className='register-input' name= "username" type="text" value={username} onChange={
                     (e) => setUsername(e.target.value)
                 }/>
-                <div className="register-subtitle">
+                <div className="register-subtitle mb-1">
                     <p>Email</p>
                 </div>
                 <input className="register-input" name= "email" type="text" value={email} onChange={
@@ -27,11 +28,17 @@ export default function RegisterForm(){
                 <div>
 
                 </div>
-                <div className="register-subtitle">
+                <div className="register-subtitle mb-1">
                     <p>Password</p>
                 </div>
                 <input className="register-input" name= "password" type="password" value={password} onChange={
                     (e) => setPassword(e.target.value)
+                }/>
+                <div className="register-subtitle mb-1">
+                    <p>Confirm password</p>
+                </div>
+                <input className="register-input" name= "password" type="password" value={confirm} onChange={
+                    (e) => setConfirm(e.target.value)
                 }/>
                 <button className="register-button">Sign up</button>
             </div>
